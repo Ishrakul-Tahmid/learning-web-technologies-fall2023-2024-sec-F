5b966b19f4982fe119f4a6250e9de9deafacfacc
Ishrakul Tahmid <ishrakultahmid@gmail.com>
2023-10-09 16:46:00 +0600

    Add files via upload

---
+++ name.php
@@ -0,0 +1,22 @@
+<html>
+
+<head>
+	<title>Name</title>
+</head>
+<body>
+	<form method="post">
+	<fieldset>
+		<legend>NAME</legend>
+		name: <input type="text" name="name" value="<?php>
+if(isset($_POST['submit']))
+{
+	$name = $_POST['name']
+	echo $name;
+}
+?>" />
+		<hr />
+            <input type="submit" name="submit" value="Submit" />
+	</fieldset>
+	</form>
+</body>
+</html>
